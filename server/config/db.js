c5e4a2a48864a2ddb55e019d7b26d74549d7eb23
jque4 <jque4@my.centennialcollege.ca>
2022-10-29 23:16:21 -0400

    QoL and formatting fixes

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,7 @@
 module.exports = {
+  // Cloud MongoDB deployment -> 
+  "URI": "mongodb+srv://admin:admin@mongodbserver.trvyp5y.mongodb.net/books229?retryWrites=true&w=majority"
+
   //local MongoDB deployment ->
-  "URI": "mongodb://127.0.0.1/books229"
-  //"URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://127.0.0.1/books229"
 };
